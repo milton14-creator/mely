8defb636cfde4ee31d1b7aa35e5b73360d16ffeb
milton14-creator <miltonmau14@gmail.com>
2025-10-23 15:35:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const mensajes = document.querySelectorAll(".mensaje");
+  let index = 0;
+
+  setInterval(() => {
+    mensajes[index].classList.remove("visible");
+    index = (index + 1) % mensajes.length;
+    mensajes[index].classList.add("visible");
+  }, 7000);
+});
 
